5931333e670d4c6197cbd68023f842aa63276b72
Eddcakes <eddadamson@gmail.com>
2025-12-06 20:30:33 +0000

    chore: remove notes, add deploy workflow, use env for path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/pkmn-tools" : "",
+  basePath: process.env.PAGES_BASE_PATH,
   images: {
     unoptimized: true
   },
